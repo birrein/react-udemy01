d92938f0669a9486c97be94e4110f43fc62032ab
Manuel Marín <birrein@gmail.com>
2022-02-18 17:59:30 -0300

    Just for change pc

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,6 +6,7 @@ import {
   Route,
 } from 'react-router-dom';
 import logo from '../logo.svg';
+import { routes } from './routes';
 
 interface Props {}
 
@@ -16,39 +17,25 @@ export const Navigation = (props: Props) => {
         <nav>
           <img src={logo} alt="React logo" />
           <ul>
-            <li>
-              <NavLink
-                to="/home"
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about"
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/users"
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                Users
-              </NavLink>
-            </li>
+            {routes.map(({ to, name }) => (
+              <li key={to}>
+                <NavLink
+                  to={to}
+                  className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
 
         <Routes>
-          <Route path="about" element={<h1>About page</h1>} />
-          <Route path="users" element={<h1>Users page</h1>} />
-          <Route path="home" element={<h1>Home page</h1>} />
+          {routes.map(({ path, Component }) => (
+            <Route path={path} element={<Component />} key={path} />
+          ))}
 
-          <Route path="/*" element={<Navigate to="/home" replace />} />
+          <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
         </Routes>
       </div>
     </BrowserRouter>
