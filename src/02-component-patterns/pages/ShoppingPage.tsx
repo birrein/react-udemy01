c444dc771fec68347f60f42424047a81df4f6b69
Manuel Marín <birrein@gmail.com>
2022-02-20 20:24:08 -0300

    some styles to test the styles

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -24,16 +24,19 @@ export const ShoppingPage = () => {
           flexWrap: 'wrap',
         }}
       >
-        <ProductCard product={product}>
-          <ProductImage />
-          <ProductTitle />
-          <ProductButtons />
-        </ProductCard>
-
         <ProductCard product={product} className="bg-dark text-white">
           <ProductCard.Image className="custom-image" />
           <ProductCard.Title className="text-bold" />
           <ProductCard.Buttons className="custom-buttons" />
+        </ProductCard>
+
+        <ProductCard product={product} className="bg-dark text-white">
+          <ProductImage
+            className="custom-image"
+            style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+          />
+          <ProductTitle className="text-bold" />
+          <ProductButtons className="custom-buttons" />
         </ProductCard>
 
         <ProductCard
@@ -42,9 +45,16 @@ export const ShoppingPage = () => {
             backgroundColor: '#70d1f8',
           }}
         >
-          <ProductImage />
-          <ProductTitle />
-          <ProductButtons />
+          <ProductImage
+            style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+          />
+          <ProductTitle style={{ fontWeight: 'bold' }} />
+          <ProductButtons
+            style={{
+              display: 'flex',
+              justifyContent: 'end',
+            }}
+          />
         </ProductCard>
       </div>
     </div>
