7b13a5ccabdad223f582e4ef39dd05bb5e161ae2
Manuel Marín <birrein@gmail.com>
2022-02-27 21:49:55 -0300

    Clean code for pre npm class

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -6,7 +6,6 @@ import {
 } from '../components';
 
 import { products } from '../data/products';
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -19,7 +18,6 @@ export const ShoppingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
           maxCount: 10,
@@ -27,19 +25,9 @@ export const ShoppingPage = () => {
       >
         {({ reset, count, isMaxCountReached, increaseBy }) => (
           <>
-            <ProductImage
-              className="custom-image"
-              style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-            />
-            <ProductTitle className="text-bold" />
-            <ProductButtons className="custom-buttons" />
-
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(2)}>+2</button>
-            )}
-            <span>{count}</span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
